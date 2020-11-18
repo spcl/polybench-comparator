b6f617c3a34b22cd415d44ed999214e01b915df0
Johannes de Fine Licht <johannes@musicmedia.dk>
2020-11-18 15:48:27 +0100

    Expose comparison as function that can be imported

--- comparator.py
+++ comparator.py
@@ -28,84 +28,96 @@ VALUE_ATOL = 1e-5
 OVERALL_ATOL = 1e-4
 ACCEPTABLE_ERROR_PERCENTAGE = 25.0
 
+
 def usage():
     print("USAGE: comparator.py [-i] <REFERENCE FILE> <OUTPUT FILE>")
     print("-i ignores newlines")
     sys.exit(2)
-    
-if len(sys.argv) < 3 or len(sys.argv) > 4:
-    usage()
 
-if sys.argv[1] == '-i':
-    if len(sys.argv) != 4:
+
+def compare(ref_file, cmp_file, ignore_newlines=False):
+    """
+    Compares a reference and result file for a Polybench experiment.
+
+    :return: True if the files are identical within a tolerance,
+             False otherwise.
+    """
+
+    total_error = 0.0
+    num_errors = 0
+    num_values = 0
+
+    with open(ref_file, 'rb') as rf:
+        with open(cmp_file, 'rb') as cf:
+            rlines = rf.readlines()
+            clines = cf.readlines()
+
+            # Ignore newlines by pasting all the lines together
+            if ignore_newlines:
+                rlines = [b' '.join(rlines)]
+                clines = [b' '.join(clines)]
+
+            if len(rlines) != len(clines):
+                print('ERROR: Line count mismatch! (%d != %d)' % (len(rlines), len(clines)))
+                return False
+
+            # Compare each line
+            for lnum, (rline, cline) in enumerate(zip(rlines, clines)):
+                rtoks = rline.split()
+                ctoks = cline.split()
+                if len(rtoks) != len(ctoks):
+                    print('ERROR: Row length mismatch at line %d' % (lnum + 1))
+                    return False
+
+                # Compare each token
+                for i, (rtok, ctok) in enumerate(zip(rtoks, ctoks)):
+                    try:
+                        refval = float(rtok)
+                    except:  # String comparison
+                        if rtok != ctok:
+                            print('ERROR: Non-numeric token mismatch at ' + '(%d, %d): %s != %s' %
+                                  (lnum + 1, i + 1, rtok, ctok))
+                            return False
+                        continue
+                    # Float comparison
+                    try:
+                        cmpval = float(ctok)
+                    except:
+                        print('ERROR: Token type mismatch at ' + '(%d, %d): %s != %s' % (lnum + 1, i + 1, rtok, ctok))
+                        return False
+                    diff = abs(refval - cmpval)
+                    total_error += diff
+
+                    if diff >= VALUE_ATOL:
+                        num_errors += 1
+                    num_values += 1
+
+    if num_values == 0:
+        print('ERROR: No values to compare')
+        return False
+
+    absdiff = (total_error / num_values)
+    err_percentage = (num_errors / num_values * 100.0)
+    print('Abs. diff: %.8f, errors: %d (%.1f%%)' % (absdiff, num_errors, err_percentage))
+
+    if absdiff > OVERALL_ATOL or err_percentage > ACCEPTABLE_ERROR_PERCENTAGE:
+        return False
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3 or len(sys.argv) > 4:
         usage()
-    ref_file = sys.argv[2]
-    cmp_file = sys.argv[3]
-    ignore_newlines = True
-else:
-    ref_file = sys.argv[1]
-    cmp_file = sys.argv[2]
-    ignore_newlines = False
-
-total_error = 0.0
-num_errors = 0
-num_values = 0
-
-with open(ref_file, 'rb') as rf:
-    with open(cmp_file, 'rb') as cf:
-        rlines = rf.readlines()
-        clines = cf.readlines()
-
-        # Ignore newlines by pasting all the lines together
-        if ignore_newlines:
-            rlines = [b' '.join(rlines)]
-            clines = [b' '.join(clines)]
-            
-        if len(rlines) != len(clines):
-            print('ERROR: Line count mismatch! (%d != %d)' % (len(rlines), 
-                                                              len(clines)))
-            sys.exit(3)
-
-        # Compare each line
-        for lnum, (rline, cline) in enumerate(zip(rlines, clines)):
-            rtoks = rline.split()
-            ctoks = cline.split()
-            if len(rtoks) != len(ctoks):
-                print('ERROR: Row length mismatch at line %d' % (lnum+1))
-                sys.exit(4)
-
-            # Compare each token
-            for i, (rtok, ctok) in enumerate(zip(rtoks, ctoks)):
-                try:
-                    refval = float(rtok)
-                except: # String comparison
-                    if rtok != ctok:
-                        print('ERROR: Non-numeric token mismatch at ' +
-                              '(%d, %d): %s != %s' % (lnum+1, i+1, rtok, ctok))
-                        sys.exit(5)
-                    continue
-                # Float comparison
-                try:
-                    cmpval = float(ctok)
-                except:
-                    print('ERROR: Token type mismatch at ' +
-                          '(%d, %d): %s != %s' % (lnum+1, i+1, rtok, ctok))
-                    sys.exit(6)
-                diff = abs(refval - cmpval)
-                total_error += diff
-
-                if diff >= VALUE_ATOL:
-                    num_errors += 1
-                num_values += 1
-
-if num_values == 0:
-    print('ERROR: No values to compare')
-    sys.exit(7)
-
-absdiff = (total_error / num_values)
-err_percentage = (num_errors / num_values * 100.0)
-print('Abs. diff: %.8f, errors: %d (%.1f%%)' % (absdiff, num_errors, 
-                                                err_percentage))
-
-if absdiff > OVERALL_ATOL or err_percentage > ACCEPTABLE_ERROR_PERCENTAGE:
-      sys.exit(1)
+    if sys.argv[1] == '-i':
+        if len(sys.argv) != 4:
+            usage()
+        ref_file = sys.argv[2]
+        cmp_file = sys.argv[3]
+        ignore_newlines = True
+    else:
+        ref_file = sys.argv[1]
+        cmp_file = sys.argv[2]
+        ignore_newlines = False
+    if compare(ref_file, cmp_file, ignore_newlines):
+        sys.exit(0)
+    else:
+        sys.exit(1)
